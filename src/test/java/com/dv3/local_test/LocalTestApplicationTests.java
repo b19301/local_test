cebeb52563a3a6b5bbd97d7a4d01d2f74dbbef4b
b19301 <bnine19301@gmail.com>
2024-11-14 16:57:23 +0900

    test - 첫 번째 푸시

---
+++ src/test/java/com/dv3/local_test/LocalTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dv3.local_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
